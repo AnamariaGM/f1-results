b6550a3afb77d428b87244f55d2ef952d3df527e
AnamariaGM <anamariamorogan2992@gmail.com>
2024-03-21 12:06:05 +0000

    Adding the main app.js file with the routes

---
+++ app.js
@@ -0,0 +1,37 @@
+// Loading environment variables from .env file, if available.
+require('dotenv').config();
+
+// Importing the Express framework
+const express = require('express')
+
+// Create an Express application instance.
+const app = express()
+
+// Using the PORT environment variable if available, otherwise default to port 3000.
+const port = process.env.PORT || 3000
+
+// Importing controller functions for handling the routes.
+const {
+    getAllRaces,
+    getRaceById,
+    getAllRaceResults,
+    getRaceResultsByRaceId}=require('./controllers/race-controllers')
+
+// Middleware to parse incoming JSON data.
+app.use(express.json())
+
+// Routes for retrieving races
+app.get('/api/races', getAllRaces)
+app.get('/api/races/:race_id', getRaceById)
+
+// Routes for retrieving race results
+app.get('/api/race-results', getAllRaceResults)
+app.get('/api/races/:race_id/results', getRaceResultsByRaceId)
+
+// Export the Express application instance to be used by other modules.
+module.exports=app
+
+// Starting the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
